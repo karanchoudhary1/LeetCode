3ba94d2283c4e00e1c9c2c3dc9d28a91a2e5839d
karanchoudhary1 <91266202+karanchoudhary1@users.noreply.github.com>
2023-07-02 03:26:03 +0530

    Time: 594 ms (21.37%), Space: 9.6 MB (49.08%) - LeetHub

--- 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
+++ 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int x;
-    //int dp[]
-        int dp[1<<14][20];
-
-    int f(int mask,int cs,int tot,vector<int>& v){
-        if(mask==x) {
-            return 0;
-        }
+    int dp[1<<14][20];
+    int f(int ind,int mask,int cs,int tot,vector<int>& v){
+        if(mask==x) return 0;
+        if(ind==v.size()) return 1e9;
         if(dp[mask][cs]!=-1) return dp[mask][cs];
-        int ans=1e9;
-        for(int i=0;i<v.size();i++){
-            if(mask==x) return 0;
-            if(mask&1<<i) continue;
-            int a=1e9;
-            int b=1e9;
-            if(cs+v[i]<=tot) a=f(mask|1<<i,cs+v[i],tot,v);
-             b=f(mask|1<<i,v[i],tot,v)+1;
-            ans=min(a,min(ans,b));
+        int a=f(ind+1,mask,cs,tot,v);
+        int b=1e9;
+        int c=1e9;
+        if(!(mask&(1<<ind))){
+            if(cs+v[ind]<=tot) b=f(ind+1,mask|1<<ind,cs+v[ind],tot,v);
+             c=f(0,mask|1<<ind,v[ind],tot,v)+1;
         }
-        return dp[mask][cs]=ans;
+        return dp[mask][cs]=min(a,min(b,c));
     }
     int minSessions(vector<int>& tasks, int sessionTime) {
         x=pow(2,tasks.size())-1;
-                memset(dp,-1,sizeof(dp));
-
-    return f(0,0,sessionTime,tasks)+1;
+        memset(dp,-1,sizeof(dp));
+        return f(0,0,0,sessionTime,tasks)+1;
     }
 };
