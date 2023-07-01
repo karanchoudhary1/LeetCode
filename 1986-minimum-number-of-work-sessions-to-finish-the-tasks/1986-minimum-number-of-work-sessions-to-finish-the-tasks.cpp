15f4dd4eeda9ab143ff250d4bd6f1f3e8947d1b6
karanchoudhary1 <91266202+karanchoudhary1@users.noreply.github.com>
2023-07-02 03:30:00 +0530

    Time: 854 ms (11.34%), Space: 29.6 MB (28.23%) - LeetHub

--- 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
+++ 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int x;
-    int dp[1<<14][20];
+    int dp[15][1<<14][20];
     int f(int ind,int mask,int cs,int tot,vector<int>& v){
         if(mask==x) return 0;
         if(ind==v.size()) return 1e9;
-        if(dp[mask][cs]!=-1) return dp[mask][cs];
+        if(dp[ind][mask][cs]!=-1) return dp[ind][mask][cs];
         int b=1e9;
         if(!(mask&(1<<ind))){
             if(cs+v[ind]<=tot) b=f(ind+1,mask|1<<ind,cs+v[ind],tot,v);
              else b=f(0,mask|1<<ind,v[ind],tot,v)+1;
         }
                 int a=f(ind+1,mask,cs,tot,v);
-        return dp[mask][cs]=min(a,b);
+        return dp[ind][mask][cs]=min(a,b);
     }
     int minSessions(vector<int>& tasks, int sessionTime) {
         x=pow(2,tasks.size())-1;
