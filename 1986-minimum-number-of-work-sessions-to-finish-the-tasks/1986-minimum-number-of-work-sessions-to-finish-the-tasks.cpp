340ce0f9109e01315ad495b477406127921c139d
karanchoudhary1 <91266202+karanchoudhary1@users.noreply.github.com>
2023-07-02 03:28:50 +0530

    Time: 260 ms (67.02%), Space: 9.5 MB (49.08%) - LeetHub

--- 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
+++ 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
@@ -6,14 +6,13 @@ public:
         if(mask==x) return 0;
         if(ind==v.size()) return 1e9;
         if(dp[mask][cs]!=-1) return dp[mask][cs];
-        int a=f(ind+1,mask,cs,tot,v);
         int b=1e9;
-        int c=1e9;
         if(!(mask&(1<<ind))){
-            if(cs+v[ind]<=tot) b=f(0,mask|1<<ind,cs+v[ind],tot,v);
-             c=f(0,mask|1<<ind,v[ind],tot,v)+1;
+            if(cs+v[ind]<=tot) b=f(ind+1,mask|1<<ind,cs+v[ind],tot,v);
+             else b=f(0,mask|1<<ind,v[ind],tot,v)+1;
         }
-        return dp[mask][cs]=min(a,min(b,c));
+                int a=f(ind+1,mask,cs,tot,v);
+        return dp[mask][cs]=min(a,b);
     }
     int minSessions(vector<int>& tasks, int sessionTime) {
         x=pow(2,tasks.size())-1;
