353423261ad7af57760694d5cb618d84653f756e
karanchoudhary1 <91266202+karanchoudhary1@users.noreply.github.com>
2023-07-29 17:21:31 +0530

    Time: 12 ms (49.05%), Space: 138.5 MB (13.31%) - LeetHub

---
+++ 0808-soup-servings/0808-soup-servings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double f(int x,int y,vector<vector<double>>& dp){
+        if(x<=0 && y<=0) return 0.5;
+        if(x<=0) return 1;
+        if(y<=0) return 0;
+        if(dp[x][y]!=-1.0) return dp[x][y];
+        double ans=0;
+        ans+=0.25*f(x-100,y,dp);
+        ans+=0.25*f(x-75,y-25,dp);
+        ans+=0.25*f(x-50,y-50,dp);
+        ans+=0.25*f(x-25,y-75,dp);
+        return dp[x][y]=ans;
+    }
+    double soupServings(int n) {
+        if(n>4800) return 1.0;
+        vector<vector<double>> dp(n+1,vector<double>(n+1,-1.0));
+        return f(n,n,dp);
+    }
+};
