93c46f1fe0e88519e186488dcc172ddd019ae80f
karanchoudhary1 <91266202+karanchoudhary1@users.noreply.github.com>
2023-08-01 15:27:30 +0530

    Added solution - LeetHub

---
+++ Copy Set Bits in Range - GFG/copy-set-bits-in-range.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String line[] = in.readLine().trim().split("\\s+");
+            int x = Integer.parseInt(line[0]);
+            int y = Integer.parseInt(line[1]);
+            int l = Integer.parseInt(line[2]);
+            int r = Integer.parseInt(line[3]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.setSetBit(x, y, l, r));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int setSetBit(int x, int y, int l, int r){
+        // code here
+        int z=0,diff=r-l+1;
+        l=l-1;
+        while(diff>0){
+            z=z<<1;
+            z=z|1;
+            diff--;
+        }
+        while(l>0){
+            z=z<<1;
+            l--;
+        }
+        return x|(y&z);
+    }
+}
