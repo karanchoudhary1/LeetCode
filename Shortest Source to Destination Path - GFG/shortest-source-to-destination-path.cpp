5db6a79637bd89360a65b0e216d768560a6b9e87
karanchoudhary1 <91266202+karanchoudhary1@users.noreply.github.com>
2023-08-03 02:06:01 +0530

    Added solution - LeetHub

--- Shortest Source to Destination Path - GFG/shortest-source-to-destination-path.cpp
+++ Shortest Source to Destination Path - GFG/shortest-source-to-destination-path.cpp
@@ -9,26 +9,25 @@ using namespace std;
 
 class Solution {
   public:
-      int shortestDistance(int N, int M, vector<vector<int>> A, int X, int Y) {
-        if(A[0][0]=0 || A[X][Y]==0) return -1;
-        queue<pair<int,pair<int,int>>> pq;
-        int dx1[4]={0,0,1,-1};
-        int dx2[4]={1,-1,0,0};
-        vector<vector<int>> dis(N,vector<int>(M,1e9));
+  int dx1[4]={0,0,1,-1};
+  int dx2[4]={1,-1,0,0};
+    int shortestDistance(int N, int M, vector<vector<int>> A, int X, int Y) {
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
         pq.push({0,{0,0}});
+        vector<vector<int>> dis(N,vector<int>(M,1e9));
+        pair<int,int> we={X,Y};
         dis[0][0]=0;
         while(pq.size()!=0){
-            auto it=pq.front();
+            auto it=pq.top();
             pq.pop();
             int d=it.first;
-            pair<int,int> we={X,Y};
             if(it.second==we) return d;
             for(int i=0;i<4;i++){
                 int nr=it.second.first+dx1[i];
                 int nc=it.second.second+dx2[i];
                 if(nr>=0 && nc>=0 && nr<N && nc<M && A[nr][nc]==1 && dis[nr][nc]>dis[it.second.first][it.second.second]+1){
-                    dis[nr][nc]=dis[it.second.first][it.second.second]+1;
                     pq.push({d+1,{nr,nc}});
+                    dis[nr][nc]=dis[it.second.first][it.second.second]+1;
                 }
             }
         }
