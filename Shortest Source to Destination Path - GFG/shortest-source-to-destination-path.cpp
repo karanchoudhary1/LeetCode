5deeed45aba0097284743d65c64d7fbda6ea2030
karanchoudhary1 <91266202+karanchoudhary1@users.noreply.github.com>
2023-08-03 02:02:41 +0530

    Added solution - LeetHub

---
+++ Shortest Source to Destination Path - GFG/shortest-source-to-destination-path.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+      int shortestDistance(int N, int M, vector<vector<int>> A, int X, int Y) {
+        if(A[0][0]=0 || A[X][Y]==0) return -1;
+        queue<pair<int,pair<int,int>>> pq;
+        int dx1[4]={0,0,1,-1};
+        int dx2[4]={1,-1,0,0};
+        vector<vector<int>> dis;
+        for(int i=0;i<N;i++){
+            vector<int> di;
+            for(int j=0;j<M;j++) di.push_back(1e5);
+            dis.push_back(di);
+        }
+        pq.push({0,{0,0}});
+        dis[0][0]=0;
+        while(pq.size()!=0){
+            auto it=pq.front();
+            pq.pop();
+            int d=it.first;
+            pair<int,int> we={X,Y};
+            if(it.second==we) return d;
+            for(int i=0;i<4;i++){
+                int nr=it.second.first+dx1[i];
+                int nc=it.second.second+dx2[i];
+                if(nr>=0 && nc>=0 && nr<N && nc<M && A[nr][nc]==1 && dis[nr][nc]>dis[it.second.first][it.second.second]+1){
+                    dis[nr][nc]=dis[it.second.first][it.second.second]+1;
+                    pq.push({d+1,{nr,nc}});
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M, x, y;
+        cin >> N >> M;
+        vector<vector<int>> v(N, vector<int>(M));
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++) cin >> v[i][j];
+        cin >> x >> y;
+        Solution ob;
+        cout << ob.shortestDistance(N, M, v, x, y) << "\n";
+    }
+}
+// } Driver Code Ends
