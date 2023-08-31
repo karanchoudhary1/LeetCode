8b3e23baa52c9cb7ba2db6b5250c2f2f9f3ee2df
karanchoudhary1 <91266202+karanchoudhary1@users.noreply.github.com>
2023-08-31 11:00:53 +0530

    Time: 15 ms (70.30%), Space: 26.4 MB (55.92%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -16,20 +16,17 @@ public:
     TreeNode* f(int i,int j,vector<int>& v1,vector<int>& v2){
          if(i>j) return NULL;
          TreeNode* curr=new TreeNode(v2[prev]);
+        int ind=m[v2[prev]];
         prev--;
-        int ind;
-        for(int z=i;z<=j;z++){
-            if(v2[prev+1]==v1[z]){
-                ind=z;
-                break;
-            }
-        }
+        if(ind>=i && ind<=j){
         curr->right=f(ind+1,j,v1,v2);
         curr->left=f(i,ind-1,v1,v2);
+        }
         return curr;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
          prev=inorder.size()-1;
+        for(int i=0;i<inorder.size();i++) m[inorder[i]]=i;
         return f(0,inorder.size()-1,inorder,postorder);
     }
 };
