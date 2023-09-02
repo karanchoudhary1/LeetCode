a8b94716dc623c8a9f813ff19f107b3fcef07af8
karanchoudhary1 <91266202+karanchoudhary1@users.noreply.github.com>
2023-09-03 03:30:28 +0530

    Time: 660 ms (10.00%), Space: 166.1 MB (30.00%) - LeetHub

--- 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.cpp
+++ 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-    int mod = 1e9+7;
-    map<pair<pair<long long,long long>,long long>,int>dp;
-    long long ans(vector<int>p,int i,int max_beauty,int k)
-    {
-        if(i == p.size())
-        {
-            if(max_beauty == 0&& k == 0)
-                return 1;
+    int mod=1e9+7;
+    map<pair<pair<long long,long long>,long long>,int> dp;
+    long long int f(int i,int k,long long int maxl,string& s,vector<int>& v){
+        if(i==s.length()){
+            if(k==0 && maxl==0) return 1;
             return 0;
         }
-        if(dp.find({{i,k},max_beauty}) != dp.end())
-            return dp[{{i,k},max_beauty}]%mod;
-        long long nontake = ans(p,i+1,max_beauty,k);
-        long long take = 0;
-        if(p[i]>0 && max_beauty>=p[i]&&k>0)
-        take = (p[i]%mod*ans(p,i+1,max_beauty-p[i],k-1)%mod)%mod;
-return  dp[{{i,k},max_beauty}] = (take%mod+nontake%mod)%mod;        
+       if(dp.find({{i,k},maxl}) != dp.end())
+            return dp[{{i,k},maxl}]%mod;
+        long long int nt=f(i+1,k,maxl,s,v);
+        long long int tk=0;
+        if(maxl-v[s[i]-'a']>=0 && k>0){
+            tk=(v[s[i]-'a']*(f(i+1,k-1,maxl-v[s[i]-'a'],s,v)%mod))%mod;
+        }
+        return dp[{{i,k},maxl}]=(tk%mod+nt%mod)%mod;
     }
     int countKSubsequencesWithMaxBeauty(string s, int k) {
-        if(k>26)
-            return 0;
-        vector<int>p(26,0);
-        for(int i = 0;i<s.length();i++)
-            p[s[i]-'a']++;
-        
-        vector<int>temp = p;
-        sort(temp.begin(),temp.end());
-    int x = k,max_beauty = 0,l = 25;
-        while(x--)
-        {
-            max_beauty+=temp[l];
-            l--;
-        }
-        return ans(p,0,max_beauty,k)%mod;
+        if(k>26) return 0;
+        vector<int> cnt(26,0);
+        for(int i=0;i<s.length();i++) cnt[s[i]-'a']++;
+        vector<int> v=cnt;
+        sort(v.begin(),v.end());
+        reverse(v.begin(),v.end());
+        long long int maxl=0;
+        for(int i=0;i<k;i++) maxl+=v[i];
+        unordered_set<char> st;
+        for(int i=0;i<s.length();i++) st.insert(s[i]);
+        string s1;
+        for(auto x:st) s1+=x;
+        return f(0,k,maxl,s1,cnt)%mod;
     }
 };
